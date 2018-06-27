cafda62256cc3c17a9372f87be4cb81664d03dbf
МакЛарен <maklaren@192.168.0.13>
2018-06-27 12:53:32 +0300

    begin

---
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by МакЛарен on 27.06.2018.
+//  Copyright © 2018 Asset Ryskul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
